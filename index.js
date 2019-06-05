8b8b0e1db688383e89fecacdca5ca9c6374b3916
Dana Banana <dana140198@gmail.com>
2019-06-05 21:24:31 +0900

    fix index.js

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ var hidden = document.getElementById("hidden");
 var animated = document.getElementById("animated");
 var cont = document.getElementsByClassName("control")[0];
 
+// Object.defineProperty(animated, 'number', {
+//     get() {
+//       return b;
+//     },
+//     set(arg) {
+//       b = arg + 2;
+//     }
+//   })
 
 cont.addEventListener('click', function(e) {
     const { target } = e;
       if (!target.id) {
         e.stopPropagation();
     } else {
-        if(target.id == 'animated' || target.id == 'hidden') {
-            p.setMod(target.id);
+        if(target.id == 'animated') {
+            console.log(target.checked);
         }
         else if(target.id == 'valueInput') {
             p.setValue(target.id);
