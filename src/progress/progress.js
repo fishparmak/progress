f10535e9ffd5b8165a361854ecdfa43544ba2034
Dana Banana <dana140198@gmail.com>
2019-06-06 00:43:10 +0900

    add comments to progress.js

--- src/progress/progress.js
+++ src/progress/progress.js
@@ -1,53 +1,53 @@
-(function(global, factory){
-    'use strict';
+(function(global, factory){ // Создание JS модуля для API
+    'use strict'; //strict mode
    
-    global.Progress = factory;
+    global.Progress = factory; //присоединить апи к global (в нашем случае - к Window)
    })(this, function () {
-       return new Progress(parentNode = [], state = 'normal', prm = '');
-       function Progress(parentNode= [], state = 'normal', prm = '') {
-           this.state = state;
-           this.prm;
-           this.value;
-           this.parentNode = parentNode;
-           var progressRunner;
-           var progressBlock;
+       return new Progress(parentNode = [], state = 'normal'); // возвращаем новую сущность Progress
+       function Progress(parentNode= [], state = 'normal') {
+           this.state = state; //состояние чарта
+           this.value; // значение прогресса в процентах
+           this.parentNode = parentNode; // node, внутри которого создаем наш чарт
+           var progressRunner; // бегунок, отображающий значение диаграммы
+           var progressBlock; //вся диаграмма
 
-           this.drawChart = function (rad=9) {
-               var circumference = 2*Math.PI*rad;
-               var strokeLength = (this.value*circumference)/100;
+           this.drawChart = function (rad=9) { // метод для рендеринга диаграммы
+               var circumference = 2*Math.PI*rad; // расчитываем длину окружности по небезыствестной формуле
+               var strokeLength = (this.value*circumference)/100; // сколько нужно заполнить, чтобы отобразить процент
                this.parentNode[0].innerHTML = '<svg class="progress" viewbox="0 0 20 20" width="200" height="200">' +
                '<circle class="progress__field" stroke="#bfbfbf" stroke-width="1" fill="none" cx="10" cy="10" r='+rad+' />' +
                '<circle class="progress__runner" stroke="#fce112" stroke-width="1" stroke-dasharray="' + strokeLength +',' + circumference + '" stroke-linecap="round" fill="none" cx="10" cy="10" r='+rad+' /></svg>';
-               var child = this.parentNode[0].childNodes[0];
-                if(child.className.baseVal=='progress'){
+               // в место назначения добавляем html код (рендерим саму диаграмму)    
+               var child = this.parentNode[0].childNodes[0]; // достаем детей в родительском ноде(диаграмму и бегунок)
+                if(child.className.baseVal=='progress'){  // чтобы не затронуть другие диаграммы вне родительского нода(использующие эту же библиотеку) 
                     progressRunner = child.childNodes[1];
                     progressBlock = child;
                 }
-                animate();
-               return this;
+                animate(); // добавляем класс в html код для создания анимации
+               return this; // возвращаем эту же сущность (Progress) для возможности использования цепи(ex: p.drawChart().setMod('animated))
            }
-           this.setMod = function (state, prm) {
-               state = state.toLowerCase();
-               this.prm = prm.toLocaleLowerCase();
+           this.setMod = function (state, prm) { // метод для установки состояния
+               state = state.toLowerCase(); 
+               prm = prm.toLocaleLowerCase();
               switch(state){
                   case 'animated':
-                      progressBlock.style.display = 'inline-block';
-                      if(this.prm=='yes'){
+                      progressBlock.style.display = 'inline-block'; // отображаем диаграмму
+                      if(prm=='yes'){
                           this.state = state;
-                          this.drawChart();
+                          this.drawChart(); //рендерим анимацию
                         }
                         else {
                             progressBlock.style.display = 'inline-block';
-                            this.state='normal'
+                            this.state='normal' // переводим диаграмму в 'нормальное' состояние, в котором можно регулировать значение
                         }
                         break;
                   case 'hidden':
-                      if(this.prm=='yes') {
-                        progressBlock.style.display = 'none';
+                      if(prm=='yes') {
+                        progressBlock.style.display = 'none'; // скрываем видимость нашей диаграммы
                         this.state = state;
                       }
                       else {
-                        progressBlock.style.display = 'inline-block';
+                        progressBlock.style.display = 'inline-block'; // отображаем диаграмму
                       }
                       break;
                   default: throw('Error');
@@ -55,22 +55,22 @@
                return this;
            }
            this.setValue = function (value) {
-               if(this.state != 'normal') {
-                   throw('Value can be changed only in the Normal state.')
+               if(this.state != 'normal') { // проверяем в нормальном ли состоянии (так как мы не можем менять значение в других случаях)
+                   throw('Value can be changed only in the Normal state.') 
                    return this;
                }
-               if(value>=0 && value<=100) {
+               if(value>=0 && value<=100) { // проверяем валидность значения
                     this.value = value;
                     this.drawChart();
+               }
+               else {
+                   throw('Invalid Value');
                }
                 return this;
            }
            var animate = function() {
-               progressRunner.classList.add('progress__runner_animated');
+               progressRunner.classList.add('progress__runner_animated'); // добавляем класс в html для создания анимации
                
-           }
-           var hideAnimation = function () {
-               progressRunner.classList.remove('progress__runner_animated');
            }
        }
    })
